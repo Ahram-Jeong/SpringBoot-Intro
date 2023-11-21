b4668ceaa0f1969470f878de5b61c97a0e6a7437
Ahram-Jeong <ahram1215@gmail.com>
2023-11-21 16:38:30 +0900

    Initial commit

---
+++ src/main/java/ash/helloboot/HellobootApplication.java
@@ -0,0 +1,31 @@
+package ash.helloboot;
+
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HellobootApplication {
+
+	public static void main(String[] args) {
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					resp.setStatus(200); // 상태 코드
+					resp.setHeader("Content-Type", "text/plain"); // header
+					resp.getWriter().println("Hello Servlet"); // body
+				}
+			}).addMapping("/hello");
+        });
+		webServer.start();
+	}
+
+}
