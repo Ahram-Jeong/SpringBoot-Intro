b9645fb12d3f40edd94adf26ac6ef9774a850960
Ahram-Jeong <ahram1215@gmail.com>
2023-11-24 16:39:05 +0900

    20231124 - 자바 코드 구성 정보 사용

--- src/main/java/ash/helloboot/HellobootApplication.java
+++ src/main/java/ash/helloboot/HellobootApplication.java
@@ -4,14 +4,26 @@ package ash.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@Configuration // 전체 App 구성 정보
 public class HellobootApplication {
+	@Bean
+	public HelloController helloController(HelloService helloService) {
+		return new HelloController(helloService);
+	}
+
+	@Bean
+	public HelloService helloService() { // DI 할 경우, 인터페이스 타입으로 리턴하세요
+		return new SimpleHelloService();
+	}
 
 	public static void main(String[] args) {
 		// Spring 컨테이너를 만드는 작업
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
 			@Override
 			protected void onRefresh() { // Spring 컨테이너 초기화
 				super.onRefresh();
@@ -26,8 +38,7 @@ public class HellobootApplication {
 				webServer.start();
 			}
 		};
-		applicationContext.registerBean(HelloController.class);
-		applicationContext.registerBean(SimpleHelloService.class);
+		applicationContext.register(HellobootApplication.class);
 		applicationContext.refresh();
 	}
 
