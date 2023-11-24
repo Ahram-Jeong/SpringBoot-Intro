98d066291f8797834aeb8bdb9fc96f678fdbf974
Ahram-Jeong <ahram1215@gmail.com>
2023-11-24 16:24:03 +0900

    20231124 - 스프링 컨테이너로 통합

--- src/main/java/ash/helloboot/HellobootApplication.java
+++ src/main/java/ash/helloboot/HellobootApplication.java
@@ -11,19 +11,24 @@ public class HellobootApplication {
 
 	public static void main(String[] args) {
 		// Spring 컨테이너를 만드는 작업
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+			@Override
+			protected void onRefresh() { // Spring 컨테이너 초기화
+				super.onRefresh();
+
+				// Servlet 컨테이너를 코드로 실행하면서 Servlet을 등록하는 작업
+				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				WebServer webServer = serverFactory.getWebServer(servletContext -> {
+					servletContext.addServlet("dispatcherServlet",
+							new DispatcherServlet(this)
+					).addMapping("/*");
+				});
+				webServer.start();
+			}
+		};
 		applicationContext.registerBean(HelloController.class);
 		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.refresh();
-
-		// Servlet 컨테이너를 코드로 실행하면서 Servlet을 등록하는 작업
-		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("dispatcherServlet",
-				new DispatcherServlet(applicationContext)
-			).addMapping("/*");
-        });
-		webServer.start();
 	}
 
 }
