a482b6ec4e894d11cdb02af3adf133a58fa600a0
Ahram-Jeong <ahram1215@gmail.com>
2023-11-25 19:31:06 +0900

    20231125 - SpringBootApplication

--- src/main/java/ash/helloboot/HellobootApplication.java
+++ src/main/java/ash/helloboot/HellobootApplication.java
@@ -1,13 +1,12 @@
 package ash.helloboot;
 
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration // 전체 App 구성 정보
@@ -24,6 +23,6 @@ public class HellobootApplication {
 	}
 
 	public static void main(String[] args) {
-		MySpringApplication.run(HellobootApplication.class, args);
+		SpringApplication.run(HellobootApplication.class, args);
 	}
 }
