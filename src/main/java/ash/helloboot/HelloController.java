b16b788ea30515276e97020ab1cfa3528fb7bf4e
Ahram-Jeong <ahram1215@gmail.com>
2023-11-25 18:58:08 +0900

    20231125 - 메소드 레벨에서의 Bean 등록

--- src/main/java/ash/helloboot/HelloController.java
+++ src/main/java/ash/helloboot/HelloController.java
@@ -1,8 +1,6 @@
 package ash.helloboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
--- src/main/java/ash/helloboot/HellobootApplication.java
+++ src/main/java/ash/helloboot/HellobootApplication.java
@@ -4,6 +4,7 @@ package ash.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -12,6 +13,16 @@ import org.springframework.web.servlet.DispatcherServlet;
 @Configuration // 전체 App 구성 정보
 @ComponentScan
 public class HellobootApplication {
+	@Bean
+	public ServletWebServerFactory servletWebServerFactory() {
+		return new TomcatServletWebServerFactory();
+	}
+
+	@Bean
+	public DispatcherServlet dispatcherServlet() {
+		return new DispatcherServlet();
+	}
+
 	public static void main(String[] args) {
 		// Spring 컨테이너를 만드는 작업
 		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
@@ -20,11 +31,13 @@ public class HellobootApplication {
 				super.onRefresh();
 
 				// Servlet 컨테이너를 코드로 실행하면서 Servlet을 등록하는 작업
-				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+				DispatcherServlet dispatcherServlet= this.getBean(DispatcherServlet.class);
+//				dispatcherServlet.setApplicationContext(this); // 스프링 컨테이너 obj를 넘겨주는 메소드
+
 				WebServer webServer = serverFactory.getWebServer(servletContext -> {
-					servletContext.addServlet("dispatcherServlet",
-							new DispatcherServlet(this)
-					).addMapping("/*");
+					servletContext.addServlet("dispatcherServlet", dispatcherServlet)
+							.addMapping("/*");
 				});
 				webServer.start();
 			}
