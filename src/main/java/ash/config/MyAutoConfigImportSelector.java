8c1f2f9d2c93598c13afde4dc0873ee18ce3f187
Ahram-Jeong <ahram1215@gmail.com>
2023-11-29 00:02:18 +0900

    20231129 - 동적인 자동 구성 정보 등록

---
+++ src/main/java/ash/config/MyAutoConfigImportSelector.java
@@ -0,0 +1,14 @@
+package ash.config;
+
+import org.springframework.context.annotation.DeferredImportSelector;
+import org.springframework.core.type.AnnotationMetadata;
+
+public class MyAutoConfigImportSelector implements DeferredImportSelector{
+    @Override
+    public String[] selectImports(AnnotationMetadata importingClassMetadata) {
+        return new String[] {
+                "ash.config.autoconfig.DispatcherServletConfig",
+                "ash.config.autoconfig.TomcatWebServerConfig"
+        };
+    }
+}
