172b5c31ddae05c68114e3366d8385d49ef29d6f
Ahram-Jeong <ahram1215@gmail.com>
2023-11-29 03:10:58 +0900

    20231129 - 자동 구성 Annotation 적용

--- src/main/java/ash/config/autoconfig/DispatcherServletConfig.java
+++ src/main/java/ash/config/autoconfig/DispatcherServletConfig.java
@@ -1,10 +1,10 @@
 package ash.config.autoconfig;
 
+import ash.config.MyAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
+@MyAutoConfiguration
 public class DispatcherServletConfig {
     @Bean
     public DispatcherServlet dispatcherServlet() {
