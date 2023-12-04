26119a391549d4577c107afb0256f8c59aba77a1
Ahram-Jeong <ahram1215@gmail.com>
2023-12-04 22:22:35 +0900

    20231204 - @Conditional과 Condition

--- src/main/java/ash/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/ash/config/autoconfig/JettyWebServerConfig.java
@@ -4,11 +4,23 @@ import ash.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @MyAutoConfiguration
+@Conditional(JettyWebServerConfig.JettyCondition.class) // @Conditional을 사용하면, Condition 인터페이스를 구현한 클래스 지정 필수
 public class JettyWebServerConfig {
     @Bean("jettyWebServerFactory") // Bean 이름은 메소드 명이 기본 값이므로, 메소드 명이 같을 경우 충돌 -> 충돌 우려 시, 이름 값 따로 부여
     public ServletWebServerFactory servletWebServerFactory() {
         return new JettyServletWebServerFactory();
+    }
+
+    static class JettyCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return true; // true -> 이 Bean 구성 정보를 사용
+        }
     }
 }
--- src/main/java/ash/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/ash/config/autoconfig/TomcatWebServerConfig.java
@@ -4,11 +4,23 @@ import ash.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @MyAutoConfiguration
+@Conditional(TomcatWebServerConfig.TomcatCondition.class) // @Conditional을 사용하면, Condition 인터페이스를 구현한 클래스 지정 필수
 public class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
+    }
+
+    static class TomcatCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return false; // false -> 이 Bean 구성 정보를 사용 X
+        }
     }
 }
