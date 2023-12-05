e416f3cb62798142eee444087a39da24ad1e40b2
Ahram-Jeong <ahram1215@gmail.com>
2023-12-06 00:57:35 +0900

    20231206 - 커스텀 @Conditional ver.2

--- src/main/java/ash/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/ash/config/autoconfig/TomcatWebServerConfig.java
@@ -1,27 +1,16 @@
 package ash.config.autoconfig;
 
+import ash.config.ConditionalMyOnClass;
 import ash.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.core.type.AnnotatedTypeMetadata;
-import org.springframework.util.ClassUtils;
 
 @MyAutoConfiguration
-@Conditional(TomcatWebServerConfig.TomcatCondition.class) // @Conditional을 사용하면, Condition 인터페이스를 구현한 클래스 지정 필수
+@ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
-    }
-
-    static class TomcatCondition implements Condition {
-        @Override
-        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return ClassUtils.isPresent("org.apache.catalina.startup.Tomcat", context.getClassLoader());
-        }
     }
 }
