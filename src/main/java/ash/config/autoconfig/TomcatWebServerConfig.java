b4d09ca1fa4f69b79e6c71d96ef7f3d8e5d31b3d
Ahram-Jeong <ahram1215@gmail.com>
2023-12-06 12:05:04 +0900

    20231206 - 자동 구성 정보(Auto Configuration) 대체하기

--- src/main/java/ash/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/ash/config/autoconfig/TomcatWebServerConfig.java
@@ -2,6 +2,7 @@ package ash.config.autoconfig;
 
 import ash.config.ConditionalMyOnClass;
 import ash.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
+    @ConditionalOnMissingBean // 사용자 구성 정보에서 만들어진 ServletWebServerFactory가 없을 경우, 해당 Bean을 생성하는 조건
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
