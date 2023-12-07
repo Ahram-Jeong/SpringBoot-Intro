2531bad780bcca85e0adb7d89c37013c6dc9be3b
Ahram-Jeong <ahram1215@gmail.com>
2023-12-07 19:10:20 +0900

    20231207 - 자동 구성에 Environment 프로퍼티 적용

--- src/main/java/ash/helloboot/HellobootApplication.java
+++ src/main/java/ash/helloboot/HellobootApplication.java
@@ -2,10 +2,21 @@ package ash.helloboot;
 
 
 import ash.config.MySpringBootApplication;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @MySpringBootApplication
 public class HellobootApplication {
+	@Bean
+	ApplicationRunner applicationRunner(Environment env) {
+		return args -> {
+			String name = env.getProperty("my.name");
+			System.out.println("my.name : " + name);
+        };
+    }
+
 	public static void main(String[] args) {
 		SpringApplication.run(HellobootApplication.class, args);
 	}
