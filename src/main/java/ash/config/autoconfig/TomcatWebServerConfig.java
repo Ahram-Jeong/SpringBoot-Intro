5053ed9c86a64cfa7db72e1ba41786b66cab3dc5
Ahram-Jeong <ahram1215@gmail.com>
2023-12-07 19:23:24 +0900

    20231207 - 자동 구성에 Environment 프로퍼티 적용 ver.3

--- src/main/java/ash/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/ash/config/autoconfig/TomcatWebServerConfig.java
@@ -6,15 +6,16 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean // 사용자 구성 정보에서 만들어진 ServletWebServerFactory가 없을 경우, 해당 Bean을 생성하는 조건
-    public ServletWebServerFactory servletWebServerFactory() {
+    public ServletWebServerFactory servletWebServerFactory(Environment env) {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setContextPath("/app");
+        factory.setContextPath(env.getProperty("contextPath"));
         return factory;
     }
 }
