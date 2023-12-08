5a6b13b3e4ea097fd19a283213af39aab002d216
Ahram-Jeong <ahram1215@gmail.com>
2023-12-08 22:02:03 +0900

    20231208 - 프로퍼티 클래스의 분리

--- src/main/java/ash/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/ash/config/autoconfig/TomcatWebServerConfig.java
@@ -2,23 +2,33 @@ package ash.config.autoconfig;
 
 import ash.config.ConditionalMyOnClass;
 import ash.config.MyAutoConfiguration;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
-    @Value("${contextPath}")
-    String contextPath;
-
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean // 사용자 구성 정보에서 만들어진 ServletWebServerFactory가 없을 경우, 해당 Bean을 생성하는 조건
-    public ServletWebServerFactory servletWebServerFactory() {
+    public ServletWebServerFactory servletWebServerFactory(ServerProperties properties) {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setContextPath(this.contextPath);
+
+        factory.setContextPath(properties.getContextPath());
+        factory.setPort(properties.getPort());
+
         return factory;
+    }
+
+    @Bean
+    public ServerProperties serverProperties(Environment environment) {
+        ServerProperties properties = new ServerProperties();
+
+        properties.setContextPath(environment.getProperty("contextPath"));
+        properties.setPort(Integer.parseInt(environment.getProperty("port")));
+
+        return properties;
     }
 }
